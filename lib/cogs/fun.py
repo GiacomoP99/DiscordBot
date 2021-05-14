a12739a753bd6010e8562e6957b07c8bdb17a7ed
GiacomoP99 <84136671+GiacomoP99@users.noreply.github.com>
2021-05-14 21:21:27 +0200

    commit giorno 2 final

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -2,6 +2,17 @@ from discord.ext.commands import Cog
 from discord.ext.commands import command
 from random import randint
 from discord.utils import get
+from discord import Embed
+from discord import File
+import requests
+from imgurpython import ImgurClient
+import configparser
+
+# config = configparser.ConfigParser()
+# config.read('auth.ini')
+# client_id = config.get('credentials', 'client_id')
+# client_secret = config.get('credentials', 'client_secret')
+# client = ImgurClient(client_id, client_secret)
 
 
 class Fun(Cog):
@@ -14,9 +25,19 @@ class Fun(Cog):
 
     @command(name="flame")
     async def flame_marco(self, ctx):
-        member = get(self.bot.get_all_members(), id=299989967659335691)
+        member = get(self.bot.get_all_members(), id=717697262221918210)
         insulti = ["puzzi", "zitto!", "vai a fare cali", "vergogna"]
         await ctx.send(f"{member.mention} {insulti[randint(0,3)]}")
+
+    @command(name="newbt")
+    async def add_new_bt(self, ctx):
+        content = ctx.message.content.replace("!newbt", "").split(" ")
+        await ctx.message.delete()
+        embed = Embed(title="NUOVA BOUNTY TRIALS",
+                      description=f"bounty trials di {content[1]}", colour=0xFF0000)
+        embed.set_image(
+            url=str(content[2]))
+        await ctx.send(embed=embed)
 
     @Cog.listener()
     async def on_ready(self):
